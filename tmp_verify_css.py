3c831bb97ada01b21591d5f509d7d07687b3b0ae
wenxin195 <zzjj921@126.com>
2026-08-09 21:22:28 +0800

    删除临时脚本

--- tmp_verify_css.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-css = Path("_site/assets/css/main.css").read_bytes()
-print("gt", css.count(b">"))
-print("amp-gt", css.count(b"&gt;"))
-print("brand-child", b".site-header__brand>" in css)
-html = Path("_site/javascript-basic.html").read_text(encoding="utf-8")
-print("code-blocks", html.count('figure class="code-block"'))
